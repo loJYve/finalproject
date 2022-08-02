440eb54687787406e225a03e9b59a7c55213d300
GDJ47 <GDJ47@192.168.0.180>
2022-08-02 10:46:50 +0900

    mg

---
+++ bjscar/src/test/java/com/bjscar/Gdj47BjscarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bjscar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gdj47BjscarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
