57a3ff517228777e5f31bd079bf4f7fcf719a037
Swift3 <Swift3@DESKTOP-R51ITGJ>
2022-08-03 12:12:15 +0900

    MapServlet추가

---
+++ GDJ47_bjscar_final/src/main/java/com/bjscar/map/contoller/MapServlet.java
@@ -0,0 +1,17 @@
+package com.bjscar.map.contoller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MapServlet {
+	
+	@RequestMapping("/Map.do")
+	public ModelAndView MapControll() {
+		
+		
+	}
+	
+
+}
