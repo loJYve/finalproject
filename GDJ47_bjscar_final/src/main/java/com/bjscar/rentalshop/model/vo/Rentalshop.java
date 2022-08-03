bd6d98f5d027f2eaac0b0dfdd6e76581e9ba9e21
GDJ47 <GDJ47@192.168.0.198>
2022-08-03 14:36:33 +0900

    hhj 대여소 자동차 클래스 생성

---
+++ GDJ47_bjscar_final/src/main/java/com/bjscar/rentalshop/model/vo/Rentalshop.java
@@ -0,0 +1,12 @@
+package com.bjscar.rentalshop.model.vo;
+
+public class Rentalshop {
+	
+	private int rentalshopId;
+	private String rentalshopName;
+	private String retalshopAddr;
+	private int latitude;
+	private int longtitude;
+	private String bmMember;
+	
+}
--- GDJ47_bjscar_final/src/main/java/com/bjscar/vehicle/model/vo/Vihicle.java
+++ GDJ47_bjscar_final/src/main/java/com/bjscar/vehicle/model/vo/Vihicle.java
@@ -1,4 +1,4 @@
-package com.bjscar.model.vo;
+package com.bjscar.vehicle.model.vo;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
