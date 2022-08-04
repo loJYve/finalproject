8996ea170f654c3e303af9a272ca4ccf669bdc7a
Swift3 <Swift3@DESKTOP-R51ITGJ>
2022-08-04 17:16:13 +0900

    map관련 package생성, map jsp생성
    Merge branch 'main' of https://github.com/loJYve/finalproject.git into
    main

---
+++ GDJ47_bjscar_final/src/main/java/com/bjscar/rentalshop/model/vo/Rentalshop.java
@@ -0,0 +1,23 @@
+package com.bjscar.rentalshop.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Rentalshop {
+
+	private int rentalshopId;
+	private String rentalshopName;
+	private String rentalshopAddr;
+	private double latitude;
+	private double longitude;
+	private String bmMember;
+	
+	
+	
+}
