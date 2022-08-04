cf513c22fc88c88ed288095f483dc7bde465aa46
GDJ47 <GDJ47@192.168.0.180>
2022-08-04 14:27:48 +0900

    mg adminpage commit

--- GDJ47_bjscar_final/src/main/java/com/bjscar/admin/model/dao/AdminDaoImpl.java
+++ GDJ47_bjscar_final/src/main/java/com/bjscar/admin/model/dao/AdminDaoImpl.java
@@ -1,7 +1,9 @@
 package com.bjscar.admin.model.dao;
 
 import java.util.List;
+import java.util.Map;
 
+import org.apache.ibatis.session.RowBounds;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -12,9 +14,24 @@ public class AdminDaoImpl implements AdminDao {
 	
 	
 	@Override
-	public List<Member> findAll(SqlSessionTemplate session) {
+	public List<Member> selectMemberList(SqlSessionTemplate session) {
 		// TODO Auto-generated method stub
-		return session.selectList("member.findAll");
+		return session.selectList("member.selectMemberList");
+	}
+	
+	@Override
+	public List<Member> selectMemberListPage(SqlSessionTemplate session,Map param) {
+		// TODO Auto-generated method stub
+		int offSet=(int)param.get("cPage");
+		int limit=(int)param.get("numPerpage");
+		
+		return session.selectList("member.selectMemberList",null,new RowBounds((offSet-1)*limit,limit));
+	}
+	
+	@Override
+	public int selectMemberCount(SqlSessionTemplate session) {
+		// TODO Auto-generated method stub
+		return session.selectOne("member.selectMemberCount");
 	}
 
 }
--- GDJ47_bjscar_final/src/main/webapp/WEB-INF/views/common/header.jsp
+++ GDJ47_bjscar_final/src/main/webapp/WEB-INF/views/common/header.jsp
@@ -4,11 +4,13 @@
 <html>
 <head>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
 <meta charset="UTF-8">
 <title>Insert title here</title>
 </head>
 <body>
+
    <div class="container">
     <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
       <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
