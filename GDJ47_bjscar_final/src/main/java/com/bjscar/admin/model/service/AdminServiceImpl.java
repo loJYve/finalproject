1f4a3ab4cae4c956d6590c43b696d49a8e7d512f
GDJ47 <GDJ47@192.168.0.180>
2022-08-04 09:28:43 +0900

    mg

--- GDJ47_bjscar_final/src/main/java/com/bjscar/admin/model/service/AdminServiceImpl.java
+++ GDJ47_bjscar_final/src/main/java/com/bjscar/admin/model/service/AdminServiceImpl.java
@@ -2,6 +2,7 @@ package com.bjscar.admin.model.service;
 
 import java.util.List;
 
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,10 +13,14 @@ public class AdminServiceImpl implements AdminService {
 
 	@Autowired
 	private AdminDao dao;
+	
+	@Autowired
+	private SqlSessionTemplate session;
+	
 	@Override
 	public List<Member> selectMembers() {
 		// TODO Auto-generated method stub
-		return dao.findAll();
+		return dao.findAll(session);
 	}
 
 }
