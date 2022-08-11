58d81afd371df901cc83615f62afc719a7476a4a
newgd <newgd@DESKTOP-NOHRDFF>
2022-08-11 22:28:59 +0900

    BJS 0811 2228 헤더 마이페이지 수정
    Merge branch 'main' of https://github.com/loJYve/finalproject.git into
    main

--- GDJ47_bjscar_final/src/main/java/com/bjscar/mypage/controller/MypageController.java
+++ GDJ47_bjscar_final/src/main/java/com/bjscar/mypage/controller/MypageController.java
@@ -1,29 +1,33 @@
 package com.bjscar.mypage.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-//import com.bjscar.mypage.model.service.MypageService;
+import com.bjscar.member.model.vo.Member;
+import com.bjscar.mypage.model.service.MypageService;
 
 @Controller
 @RequestMapping("/mypage")
 public class MypageController {
 
-	/*
-	 * @Autowired
-	private MypageService service; */
+	@Autowired
+	private MypageService service; 
+	@Autowired
+	private HttpSession session;
+	@Autowired
+	private Member loginMember = (Member)session.getAttribute("loginMember"); 
 	
 	@RequestMapping("/rentaldata.do")
-	/*public ModelAndView rentalData(ModelAndView mv) {
-		//mv.addObject("rentalStatus", service.);
+	public ModelAndView rentalData(ModelAndView mv) {
+		mv.addObject("rentalStatus", service.selectRentalData());
 		mv.setViewName("/rentalData");
 		return mv;
-	}*/
-	public String rentalData() {
-		return "/mypage/rentalData";
 	}
+	
 	
 	@RequestMapping("/rentalhistory.do")
 	public String rentalHistory() {
