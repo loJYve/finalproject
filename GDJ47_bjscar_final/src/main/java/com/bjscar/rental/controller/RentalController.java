53f062b53358caf5a8d7f8d90b79d318b7aacd31
ChungJunYong <ChungJunYong@DESKTOP-HTL8HTM>
2022-08-16 17:05:25 +0900

    cjy_220816_1705 commit

--- GDJ47_bjscar_final/src/main/java/com/bjscar/rental/controller/RentalController.java
+++ GDJ47_bjscar_final/src/main/java/com/bjscar/rental/controller/RentalController.java
@@ -4,11 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/rental")
 public class RentalController {
 	
 	@RequestMapping("/rental.do")
 	public String rental() {
 		return "rental/rental";
+	}
+	
+	@RequestMapping("/searchVehicle.do")
+	public void searchVehicle(String vehicleGrade) {
+		
 	}
 
 }
