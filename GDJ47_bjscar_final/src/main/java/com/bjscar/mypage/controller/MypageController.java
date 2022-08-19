3e07ba9309793f378ac017a373215b48a5e4a08f
GDJ47 <GDJ47@192.168.0.172>
2022-08-19 17:13:15 +0900

    bjs 0819 1713 마이페이지 수정중
    Merge branch 'main' of https://github.com/loJYve/finalproject.git into
    main

--- GDJ47_bjscar_final/src/main/java/com/bjscar/mypage/controller/MypageController.java
+++ GDJ47_bjscar_final/src/main/java/com/bjscar/mypage/controller/MypageController.java
@@ -24,13 +24,16 @@ public class MypageController {
 	
 	@RequestMapping("/rentaldata.do")
 	public ModelAndView rentalData(@RequestParam String memberId, ModelAndView mv) {
-		mv.addObject("rentalStatus", service.selectRentalData(memberId));
+		//mv.addObject("rentalStatus", service.selectRentalData(memberId));
 		
 		try {
-			RentalHistory rh=service.selectRentalData(memberId);
-			 mv.addObject("carImage", service.selectCarImage(rh.getVehicleId()));
-			 mv.addObject("carData", service.selectCarData(rh.getVehicleId()));
-			 mv.addObject("rentalshopData", service.selectRentalshopData(rh.getVehicleId()));
+			/*
+			 * RentalHistory rh=service.selectRentalData(memberId); 
+			 * mv.addObject("carImage",service.selectCarImage(rh.getVehicleId())); 
+			 * mv.addObject("carData",service.selectCarData(rh.getVehicleId()));
+			 * mv.addObject("rentalshopData",service.selectRentalshopData(rh.getVehicleId()));
+			 */
+			mv.addObject("rentalDataList", service.selectRentalDataList(memberId));
 		} catch (NullPointerException e) {
 			
 		}
@@ -60,7 +63,7 @@ public class MypageController {
 			@RequestParam(name="numPerpage",defaultValue="5")int numPerpage, @RequestParam String memberId, ModelAndView mv) {
 		Map param=Map.of("cPage",cPage,"numPerpage",numPerpage,"memberId",memberId);	
 		mv.addObject("purchasehistory", service.selectPurchaseHistory(param));
-		
+		System.out.println(service.selectPurchaseHistory(param));
 		int totalData=service.selectPHCount(memberId);
 		mv.addObject("pageBar",PageFactory.getPageBar(totalData, numPerpage, cPage, "purchasehistory.do?memberId="+memberId));
 		
